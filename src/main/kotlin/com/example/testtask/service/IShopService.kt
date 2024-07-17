11a4b5a9f9c0a610e9d60eaf124869c36aae793f
SergeyRuzheynikov <ruzhejnikov@mt-r.ru>
2024-07-17 20:48:35 +0300

    #added purchase method into IShopService

--- src/main/kotlin/com/example/testtask/service/IShopService.kt
+++ src/main/kotlin/com/example/testtask/service/IShopService.kt
@@ -1,9 +1,12 @@
 package com.example.testtask.service
 
 import com.example.testtask.controller.CalculatePriceDto
+import com.example.testtask.controller.PurchaseDto
 
 interface IShopService {
 
     fun calculatePrice(dto: CalculatePriceDto):UInt
+
+    fun purchase(dto: PurchaseDto):String
 
 }
